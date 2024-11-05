21276b0a3fab2a1a216ec4bc6103d492c466fa65
mohamed <simonbakhcha@gmail.com>
2024-11-05 10:02:37 +0100

    torqan Dfs

--- main.go
+++ main.go
@@ -14,7 +14,9 @@ type info struct {
 	end   string
 }
 
-var Link = make(map[string][]string)
+var (
+	Link = make(map[string][]string)
+)
 
 func main() {
 	var mok info
@@ -28,8 +30,6 @@ func main() {
 
 func Parsing(fileName string, a *info) {
 	var st, fin bool
-	// stockSl := [][]string{}
-	//cords := make(map[string]bool)
 	uniRooms := make(map[string]bool)
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -40,7 +40,6 @@ func Parsing(fileName string, a *info) {
 
 	scanner := bufio.NewScanner(file)
 	i := 0
-	// st, fin := false, false
 
 	for scanner.Scan() {
 		if i == 0 {
@@ -62,7 +61,6 @@ func Parsing(fileName string, a *info) {
 		}
 		if strings.HasPrefix(scanner.Text(), "#") {
 			if scanner.Text() == "##start" {
-				//fmt.Println(stock)
 				if !st {
 					st = true
 					continue
@@ -71,7 +69,6 @@ func Parsing(fileName string, a *info) {
 					return
 				}
 
-				// st = true
 			} else if scanner.Text() == "##end" {
 				if !fin {
 					fin = true
@@ -80,17 +77,11 @@ func Parsing(fileName string, a *info) {
 					fmt.Println("ERROR*: invalid data format")
 					return
 				}
-				// fin = true
 			}
 			continue
 		}
 		ysf := strings.Fields(scanner.Text())
 		if scanner.Text() != "" {
-
-			// if len(ysf) != 3 {
-			// fmt.Println(ysf)
-			// 	return
-			// }else{
 			if len(ysf) == 3 {
 				if !uniRooms[ysf[0]] {
 					uniRooms[ysf[0]] = true
@@ -98,13 +89,14 @@ func Parsing(fileName string, a *info) {
 					fmt.Println("room meawda a 3chiri")
 					return
 				}
-			} else if len(ysf) == 1 && st && fin{
+			} else if len(ysf) == 1 && st && fin {
 				lin := strings.Split(scanner.Text(), "-")
 				if len(lin) == 2 {
-					if uniRooms[lin[0]] {
+					if uniRooms[lin[0]] { // nfekro flm3awda!
 						Link[lin[0]] = append(Link[lin[0]], lin[1])
+						Link[lin[1]] = append(Link[lin[1]], lin[0])
+
 					} else {
-						fmt.Println(scanner.Text())
 						fmt.Println("ERROR: room mam3rofach")
 						return
 					}
@@ -114,7 +106,7 @@ func Parsing(fileName string, a *info) {
 					return
 				}
 
-			}else{
+			} else {
 				fmt.Println("ERROR: invalid data format")
 				return
 			}
@@ -123,7 +115,6 @@ func Parsing(fileName string, a *info) {
 			return
 		}
 		if st && len(a.start) == 0 {
-			// stockSl = append(stockSl, ysf)
 			if len(ysf) != 3 {
 				fmt.Println("ERROR': invalid data format")
 				return
@@ -131,7 +122,6 @@ func Parsing(fileName string, a *info) {
 			if len(a.start) == 0 {
 				a.start = ysf[0]
 			}
-			// mok.start = stockSl[0][0]
 		}
 		if fin && len(a.end) == 0 {
 			if len(ysf) == 3 {
@@ -145,34 +135,58 @@ func Parsing(fileName string, a *info) {
 		fmt.Println("ERROR: invalid data format")
 		return
 	}
-	//endc , startc := false,false
-	//_, startc := Link[a.start]
-	
+
 	if !(check(a.end) && check(a.start)) {
-		fmt.Println("azeby start awla end ra mamlinkinch")
+		fmt.Println("khoya!! start awla end ra mamlinkinch")
 		return
 	}
-	
-	
 
 	fmt.Println(Link)
-	fmt.Println(a.start)
-	fmt.Println(a.end)
-	fmt.Println(uniRooms)
-
+	p := findAllPaths(a.start, a.end)
+	fmt.Println(p)
 }
-func check(s string)bool{
-	_, ok :=  Link[s]
-	if  !ok{
+func check(s string) bool {
+	_, ok := Link[s]
+	if !ok {
 		for _, v := range Link {
 			for _, mok := range v {
 				if mok == s {
 					return true
 				}
 			}
-	   }
-	}else{
+		}
+	} else {
 		return true
 	}
 	return false
 }
+
+func findAllPaths(start, end string) [][]string {
+	var paths [][]string
+	visited := make(map[string]bool)
+	dfs(start, end, visited, []string{}, &paths)
+	return paths
+}
+
+func dfs(start, end string, visited map[string]bool, currentPath []string, paths *[][]string) {
+	visited[start] = true
+	currentPath = append(currentPath, start)
+	fmt.Println(start,currentPath)
+	fmt.Println(visited)
+	//fmt.Println(currentPath)
+
+	if start == end {
+		*paths = append(*paths, append([]string{}, currentPath...))
+	} else {
+		for _, neighbor := range Link[start] {
+
+			if !visited[neighbor] {
+				dfs(neighbor, end, visited, currentPath, paths)
+			}
+		}
+	}
+
+	visited[start] = false
+	//currentPath = currentPath[:len(currentPath)-1]
+}
+
