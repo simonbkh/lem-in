4ffcc22021fdd6618215acc38c83210413a0a48d
BAKHCHA Mohamed <61326232+simonbkh@users.noreply.github.com>
2024-11-11 10:25:34 +0100

    better  me

--- main.go
+++ main.go
@@ -27,16 +27,8 @@ func main() {
 }
 
 func Parsing(fileName string, a *info) {
-	var stock string
-	// stockSl := [][]string{}
-
+	var st, fin bool
 	uniRooms := make(map[string]bool)
-	content, err := os.ReadFile(fileName)
-	if err != nil || len(content) == 0 {
-		fmt.Println("file dialk fih machkil a 3chiri")
-		return
-	}
-
 	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
@@ -46,7 +38,6 @@ func Parsing(fileName string, a *info) {
 
 	scanner := bufio.NewScanner(file)
 	i := 0
-	// st, fin := false, false
 
 	for scanner.Scan() {
 		if i == 0 {
@@ -68,81 +59,202 @@ func Parsing(fileName string, a *info) {
 		}
 		if strings.HasPrefix(scanner.Text(), "#") {
 			if scanner.Text() == "##start" {
-				fmt.Println(stock)
-				if scanner.Text() != stock {
-					stock = scanner.Text()
+				if !st && len(a.end) == 0 && !fin {
+					st = true
 					continue
 				} else {
-					fmt.Println("ERROR9: invalid data format")
+					fmt.Println("ERROR9: invalid data format", scanner.Text())
 					return
 				}
-
-				// st = true
 			} else if scanner.Text() == "##end" {
-				if scanner.Text() != stock {
-					stock = scanner.Text()
+				if !fin && len(a.start) != 0 {
+					fin = true
 					continue
 				} else {
-					fmt.Println("ERROR: invalid data format")
+					fmt.Println("ERROR*: invalid data format", scanner.Text())
 					return
 				}
-				// fin = true
 			}
 			continue
 		}
 		ysf := strings.Fields(scanner.Text())
 		if scanner.Text() != "" {
+			if len(ysf) == 3 {
+				if !uniRooms[ysf[0]] {
+					uniRooms[ysf[0]] = true
+				} else {
+					fmt.Println("room meawda a 3chiri")
+					return
+				}
+			} else if len(ysf) == 1 && st && fin {
+				lin := strings.Split(scanner.Text(), "-")
+				if len(lin) == 2 {
+					if uniRooms[lin[0]] && uniRooms[lin[1]] { // nfekro flm3awda!
+						Link[lin[0]] = append(Link[lin[0]], lin[1])
+						Link[lin[1]] = append(Link[lin[1]], lin[0])
 
-			// if len(ysf) != 3 {
-			fmt.Println(ysf)
-			// 	return
-			// }else{
-			if !uniRooms[ysf[0]] {
-				uniRooms[ysf[0]] = true
+					} else {
+						fmt.Println("ERROR: room mam3rofach", scanner.Text())
+						return
+					}
+				} else {
+					fmt.Println("ERROR$$: invalid data format")
+					return
+				}
+
 			} else {
-				fmt.Println("room meawda a 3chiri")
+				fmt.Println("ERROR: invalid data format")
 				return
 			}
+		} else {
+			fmt.Println("ERROR^: invalid data format")
+			return
 		}
-		if stock == "##start" && !strings.HasPrefix(scanner.Text(), "#") {
-			// stockSl = append(stockSl, ysf)
+		if st && len(a.start) == 0 {
 			if len(ysf) != 3 {
-				fmt.Println("ERROR: invalid data format")
+				fmt.Println("ERROR': invalid data format")
 				return
 			}
 			if len(a.start) == 0 {
 				a.start = ysf[0]
-			}else{
-				fmt.Println("ERROR: mok")
-				return
-			}
-			// mok.start = stockSl[0][0]
-		}
-		if stock == "##end" {
-				if len(a.end) == 0 && len(ysf) == 3 {
-					a.end = ysf[0]
-				}
-				if len(ysf) == 1 {
-					lin := strings.Split(scanner.Text(), "-")
-					if len(lin) == 2 {
-						Link[lin[0]] = append(Link[lin[0]], lin[1])
-					} else {
-						fmt.Println("ERROR: invalid data format")
-						return
-					}
-
-				}
-
-			} else {
-				fmt.Println("ERROR0: invalid data format")
-				return
 			}
 		}
+		if fin && len(a.end) == 0 {
+			if len(ysf) == 3 {
+				a.end = ysf[0]
+			}
+		}
+	}
 
-		i++
-	
+	if len(a.end) == 0 || len(a.start) == 0 {
+		fmt.Println("ERROR: invalid data format")
+		return
+	}
+
+	if len(a.end) == 0 && len(a.start) == 0 {
+		fmt.Println("khoya!! start awla end ra mamlinkinch")
+		return
+	}
+
 	fmt.Println(Link)
-	fmt.Println(a.start)
-	fmt.Println(a.end)
-	fmt.Println(uniRooms)
+	p := findAllPaths(a)
+	fmt.Println(p)
+
+	m := MesingPath(p)
+	fmt.Println(m)
+}
+
+func findAllPaths(m *info) [][]string {
+	var paths [][]string
+	visited := make(map[string]bool)
+	dfs(m.start, m.end, visited, []string{}, &paths, m)
+	return paths
+}
+
+func dfs(start, end string, visited map[string]bool, currentPath []string, paths *[][]string, m *info) {
+	visited[start] = true
+	currentPath = append(currentPath, start)
+	
+
+	if start == end {
+		*paths = append(*paths, append([]string{}, currentPath...))
+	} else {
+		good := false
+		if start != m.start {
+			for _, v := range Link[start] {
+				if v == end {
+					good = true
+					currentPath = append(currentPath, v)
+					*paths = append(*paths, append([]string{}, currentPath...))
+					break
+				}
+			}
+		}
+		if !good {
+			for _, neighbor := range Link[start] {
+
+				if !visited[neighbor] {
+					dfs(neighbor, end, visited, currentPath, paths, m)
+				}
+				// if neighbor == end {
+				// 	break
+				// }
+			}
+		}
+
+	}
+
+	visited[start] = false
+	// currentPath = currentPath[:len(currentPath)-1]
+}
+
+func MesingPath(paths [][]string) [][]string {
+	var pp [][]string
+	var p []int
+	var Nber int
+	for _, v := range paths {
+		for _, i := range v {
+			for _, j := range paths {
+				for _, k := range j {
+					if i == k {
+						Nber++
+					}
+				}
+			}
+		}
+		p = append(p, Nber)
+		Nber = 0
+	}
+
+	mpp := make(map[string]bool)
+	for i := 0; i < len(p); i++ {
+		index := Small(&p)
+
+		if check(paths[index][1:len(paths[index])-1], &mpp) {
+			pp = append(pp, paths[index])
+		}
+	}
+
+	// pp = append(pp, paths[index])
+	return pp
+}
+
+func Small(p *[]int) int {
+	var min int
+	var index int
+	min = (*p)[0]
+	for r, v := range *p {
+		if min == -1  && v !=-1{
+			min = v
+			index = r
+		}
+		if v == -1{
+			continue
+		}
+		if v < min  {
+			min = v
+			index = r
+		}
+	}
+	(*p)[index] = -1
+	//fmt.Println(*p)
+	return index
+}
+
+func check(path []string, mp *map[string]bool) bool {
+	temp := []string{}
+	for _, room := range path {
+		if !(*mp)[room] {
+			(*mp)[room] = true
+			temp = append(temp, room)
+		} else {
+			for _, v := range temp {
+				delete((*mp), v)
+			}
+			fmt.Println("false")
+			return false
+		}
+	}
+	fmt.Println("true")
+	return true
 }
